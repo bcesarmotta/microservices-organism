cd3e471e38a670cee2926ddc8364e3d83facdcf1
Bruno César <bcesarmotta@yahoo.com.br>
2019-07-04 17:46:14 -0300

    chore(serviceDiscovery): add adjusts to consumer

--- campaign/src/main/java/com/microservices/campaign/Application.java
+++ campaign/src/main/java/com/microservices/campaign/Application.java
@@ -2,6 +2,7 @@ package com.microservices.campaign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
