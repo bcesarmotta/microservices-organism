aac1243b5551f6c5549fddd907bf5b556844dd1d
Bruno César <bcesarmotta@yahoo.com.br>
2019-07-20 20:07:19 -0300

    chore(campaign): add parametger validations on post request

--- campaign/src/main/java/com/microservices/campaign/controller/CampaignController.java
+++ campaign/src/main/java/com/microservices/campaign/controller/CampaignController.java
@@ -4,6 +4,7 @@ import com.microservices.campaign.service.ICampaignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 import com.microservices.commons.param.CampaignParam;
 
@@ -69,7 +70,10 @@ public class CampaignController {
 
 
     public void validateBeforeSave(CampaignParam param) {
-
+        Assert.isTrue(param.getName() != null,"Parameter name must be informed");
+        Assert.isTrue(param.getInitialEffectiveDate() != null, "Initial effective date must be informed");
+        Assert.isTrue(param.getFinalEffectiveDate() != null, "Final effective date must be informed");
+        Assert.isTrue(param.getFootballTeamId() != null, "Football team ID must be informed");
     }
 
     public void validateBeforeUpdate(CampaignParam param) {
